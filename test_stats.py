6f542e7e405efd5e9e575d41f4b399111437451c
krishna <kp@wisc.edu>
2015-08-27 13:40:41 -0500

    3rd change adding nose tools

--- test_stats.py
+++ test_stats.py
@@ -1,9 +1,17 @@
-from stats import mean 
+from stats import mean
+from nose.tools import assert_equal_equal  
 def test_mean():
-	assert(mean([2,4]) == 3)
+	assert_equal(mean([2,4]), 3)
 	#print mean([2,4])
+#test_mean()
 def test_float_mean():
-	assert(mean([1,2])) == 1.5)
+	assert_equal(mean([1,2]),1.5)
+#test_float_mean()
+def test_neg_mean():
+	assert_equal(mean([-2,-4]), -3)
+test_neg_mean()
+def test_mixed_mean():
+	assert_equal(mean([-4,4]), 0)
+#test_mixed_mean()	
 
-test_mean()
-test_float_mean()
+

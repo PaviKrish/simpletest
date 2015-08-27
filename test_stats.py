8fc81e1905eb14e5908da60bbca1dbd0e1c7b641
krishna <kp@wisc.edu>
2015-08-27 13:51:17 -0500

    almost correct included

--- test_stats.py
+++ test_stats.py
@@ -1,17 +1,17 @@
 from stats import mean
-from nose.tools import assert_equal_equal  
+from nose.tools import assert_almost_equal 
 def test_mean():
-	assert_equal(mean([2,4]), 3)
+	assert_almost_equal(mean([2,4]), 3)
 	#print mean([2,4])
 #test_mean()
 def test_float_mean():
-	assert_equal(mean([1,2]),1.5)
+	assert_almost_equal(mean([1,2]),1.5)
 #test_float_mean()
 def test_neg_mean():
-	assert_equal(mean([-2,-4]), -3)
+	assert_almost_equal(mean([-2,-4]), -3)
 test_neg_mean()
 def test_mixed_mean():
-	assert_equal(mean([-4,4]), 0)
+	assert_almost_equal(mean([-4,4]), 0)
 #test_mixed_mean()	
 
 

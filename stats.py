1694f21e13013a9b151cde2b348327efb8087d5f
krishna <kp@wisc.edu>
2015-08-27 14:13:09 -0500

    first sd

--- stats.py
+++ stats.py
@@ -2,6 +2,11 @@ def mean(vals):
 	total = float(sum((vals)))
 	lenght = len(vals)
 	return total/lenght 
+def std(vals):
+	return 1.0
+	
+	
+	
 #def test_mean():
 #	assert(mean([2,4]) == 3)
 #	#print mean([2,4])
--- test_stats.py
+++ test_stats.py
@@ -1,4 +1,5 @@
-from stats import mean
+from stats import mean, std
+from nose.tools import assert_equal
 from nose.tools import assert_almost_equal 
 def test_mean():
 	assert_almost_equal(mean([2,4]), 3)
@@ -13,5 +14,7 @@ test_neg_mean()
 def test_mixed_mean():
 	assert_almost_equal(mean([-4,4]), 0)
 #test_mixed_mean()	
-
-
+def test_std1():
+	obs =std([0.0,2.0])
+	exp = 1.0 
+	assert_equal(obs,exp)
